5edd3a9f018f972dc6283f0e4b1c93b72a84056b
Jaime Martín Martín <jaimemartinmartin15@gmail.com>
2024-06-12 16:38:20 +0200

    Restructure project folders

--- src/app/commands-layout/commands-layout.routes.ts
+++ src/app/commands-layout/commands-layout.routes.ts
@@ -8,23 +8,23 @@ export const COMMANDS_LAYOUT_ROUTES: Route[] = [
     children: [
       {
         path: 'cd',
-        loadChildren: () => import('../cd/cd.routes').then(m => m.CD_ROUTES),
+        loadChildren: () => import('./cd/cd.routes').then(m => m.CD_ROUTES),
       },
       {
         path: 'find',
-        loadChildren: () => import('../find/find.routes').then(m => m.FIND_ROUTES),
+        loadChildren: () => import('./find/find.routes').then(m => m.FIND_ROUTES),
       },
       {
         path: 'host',
-        loadChildren: () => import('../host/host.routes').then(m => m.HOST_ROUTES),
+        loadChildren: () => import('./host/host.routes').then(m => m.HOST_ROUTES),
       },
       {
         path: 'if',
-        loadChildren: () => import('../if/if.routes').then(m => m.IF_ROUTES),
+        loadChildren: () => import('./if/if.routes').then(m => m.IF_ROUTES),
       },
       {
         path: 'nmap',
-        loadChildren: () => import('../nmap/nmap.routes').then(m => m.NMAP_ROUTES),
+        loadChildren: () => import('./nmap/nmap.routes').then(m => m.NMAP_ROUTES),
       },
     ]
   }
